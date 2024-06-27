2aaa31581604fff3aed10b53ad49c7162cb1bd7e
ly <2528264882@qq.com>
2024-06-27 17:29:19 +0800

    修改信息

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
     },
     app: {
         head: {
-            title: 'CF AI Web',
+            title: 'LY AI Web',
             meta: [
                 {
                     name: 'keywords',
