78426363d7983bdb8294b5c5244e3b06f4b4721b
ly <2528264882@qq.com>
2024-06-27 18:24:48 +0800

    1

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,7 +7,7 @@ import {version} from '../package.json'
     <div class="text-center text-xs font-light">
       {{ version }} | 更多精彩
       <a href="https://lyone.xyz" target="_blank"
-         class="text-neutral-500 hover:text-neutral-900 underline dark:text-neutral-400 dark:hover:text-neutral-300">GitHub</a>
+         class="text-neutral-500 hover:text-neutral-900 underline dark:text-neutral-400 dark:hover:text-neutral-300">Ly0ne</a>
     </div>
   </footer>
 </template>
